512dc6bf486b7e860469b062c9b0f19e7ce1f2f8
lon phelps <puzzleboks@gmail.com>
2015-09-17 09:17:03 -0400

    Experimenting with d3, formatting dates

--- marvel_timelines.js
+++ marvel_timelines.js
@@ -21,7 +21,7 @@ $(document).ready(function(){
         var heroId = response.data.results[0].id;
         var myImgPath = response.data.results[0].thumbnail.path + ".jpg";
         var limit = 50;
-        var startDate = response.data.results[0].start;
+
         // console.log(startDate)
         // var testUrl = response.data.results[0].events.items[0].resourceURI;
         // http://gateway.marvel.com:80/v1/public/characters/1009610/events?limit=50&apikey=e687d607d622b25c31d6ae38f2f42597
@@ -30,7 +30,7 @@ $(document).ready(function(){
         $('.heroImg').attr('src', myImgPath)
         console.log(heroId)
         // console.log(etag)
-        console.log(response)
+        // console.log(response)
         // console.log(heroEvents)
         //http://gateway.marvel.com:80/v1/public/characters/1009351/events?apikey=e687d607d622b25c31d6ae38f2f42597
         // http://gateway.marvel.com:80/v1/public/characters/1009610/events?orderBy=startDate&limit=50&apikey=e687d607d622b25c31d6ae38f2f42597
@@ -40,11 +40,27 @@ $(document).ready(function(){
           dataType: "json"
         }).done(function(res2){
           console.log(res2)
+          var data = [];
+          var startDate;
+          var format = d3.time.format("%B %d, %Y");
+          var newDate;
+          var timeline = d3.select(".timeline");
+          // var line = timeline.select(".timeline")
+          console.log(newRect);
+          // console.log(data);
           // $.format.parseDate(date);
+          // $.format.date(r2d[i].start, "MMMM, yyyy")
           var r2d = res2.data.results
           for (var i = 0; i < res2.data.count; i++){
-            $('.heroInfo').append("<li><p>" + r2d[i].title + "</p><p>" + $.format.date(r2d[i].start, "MMMM, yyyy") + "</p><p>" + r2d[i].description + "</p></li>")
+            startDate = res2.data.results[i].start;
+            newDate = format(new Date(startDate))
+            data.push(startDate)
+            $('.heroInfo').append("<li><p>" + newDate + "</p></li>")
+            // $('.heroInfo').append("<li><p>" + r2d[i].title + "</p><p>" + $.format.date(r2d[i].start, "MMMM, yyyy") + "</p><p>" + r2d[i].description + "</p></li>")
           }
+          var rect = timeline.select("rect").data(data).enter();
+          var newRect = rect.append("rect")
+          // console.log(data);
           // console.log(res2.data.results[5].description)
         })
       })
