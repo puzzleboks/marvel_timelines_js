d5f10c3a1749c7c23e1631c2c7f766ae88288748
lon phelps <puzzleboks@gmail.com>
2015-09-18 14:19:02 -0400

    Error handling and readme file

--- marvel_timelines.js
+++ marvel_timelines.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
   $('.heroInput').on('keypress', function(event) {
     if(event.which == 13){
       event.preventDefault();
+      $('.heroInput').attr({'placeholder': 'Type in the name of a Marvel superhero'});
 
       var privKey = "";
       pubKey = "e687d607d622b25c31d6ae38f2f42597";
@@ -16,38 +17,23 @@ $(document).ready(function(){
         type: "get",
         dataType: "json"
       }).done(function(response){
-        var heroId = response.data.results[0].id;
         var limit = 50;
-        // var etag = response.etag;
-        //if doesn't exist, then try again?
-        console.log(response.data.results[0].thumbnail)
-        if (response.data.results[0].thumbnail!==null){
-          var myImgPath = response.data.results[0].thumbnail.path + "/landscape_incredible.jpg";
-          // console.log('it is null');
+        //if doesn't exist, then try again
+        if (response.data.results.length!==0){
+          var heroId = response.data.results[0].id;
+          var testUrl = "http://gateway.marvel.com:80/v1/public/characters/" + heroId + "/events?orderBy=startDate" + "&limit=" + limit + "&ts=" + ts + "&apikey=e687d607d622b25c31d6ae38f2f42597&hash=" + hash;
+          if (response.data.results[0].thumbnail!==null){
+            var myImgPath = response.data.results[0].thumbnail.path + "/landscape_incredible.jpg";
+            $('.heroImg').css({'background-image': 'url(' + myImgPath + ')', 'background-repeat': 'no-repeat', 'background-position': '50% 50%'});
+          }
         }else{
           var myImgPath = "http://dummyimage.com/464x261/3b3b3b/ffffff.png&text=No+image+available";
           $('.heroInput').attr({'placeholder': 'Sorry, try a different superhero'})
+          console.log("else")
         }
 
-
-        //if image path is null
-        // var myImgPath = response.data.results[0].thumbnail.path + "/landscape_incredible.jpg";
-
-
-        // console.log(startDate)
-        // var testUrl = response.data.results[0].events.items[0].resourceURI;
-        // http://gateway.marvel.com:80/v1/public/characters/1009610/events?limit=50&apikey=e687d607d622b25c31d6ae38f2f42597
-        var testUrl = "http://gateway.marvel.com:80/v1/public/characters/" + heroId + "/events?orderBy=startDate" + "&limit=" + limit + "&ts=" + ts + "&apikey=e687d607d622b25c31d6ae38f2f42597&hash=" + hash;
-
-        // $('.heroImg').attr('src', myImgPath)
-        // $('.page-header').css('background-image', 'url(' + myImgPath + ')', 'no-repeat');
-        $('.heroImg').css({'background-image': 'url(' + myImgPath + ')', 'background-repeat': 'no-repeat', 'background-position': '50% 50%'});
         console.log(heroId)
-        // console.log(etag)
-        // console.log(response)
-        // console.log(heroEvents)
-        //http://gateway.marvel.com:80/v1/public/characters/1009351/events?apikey=e687d607d622b25c31d6ae38f2f42597
-        // http://gateway.marvel.com:80/v1/public/characters/1009610/events?orderBy=startDate&limit=50&apikey=e687d607d622b25c31d6ae38f2f42597
+
         $.ajax({
           url: testUrl,
           type: "get",
@@ -60,11 +46,6 @@ $(document).ready(function(){
           var format = d3.time.format("%B %d, %Y");
           var newDate;
           var timeline = d3.select(".timeline");
-          // var line = timeline.select(".timeline")
-          // console.log(newRect);
-          // console.log(data);
-          // $.format.parseDate(date);
-          // $.format.date(r2d[i].start, "MMMM, yyyy")
           var r2d = res2.data.results;
 
           $('timeline').empty();
@@ -74,31 +55,15 @@ $(document).ready(function(){
             newDate = format(new Date(startDate));
             data.push(startDate);
             entityIds[r2d[i].title] = r2d[i].id;
-            // <h5 class="timeline-date">Publication of first issue: 1984</h5>
             $('.timeline').append(
               "<li class='timeline_li'><div class='timeline-badge'><i class='glyphicon glyphicon-hand-left'></i></div><div class='timeline-panel'><div class='timeline-heading'><h4 class='timeline-title'>" + r2d[i].title + "</h4><h5 class='timeline-date'>First issue: " + newDate + "</h5></div><div class='timeline-body'><p>" + r2d[i].description + "</p></div><button class='btn btn-default btn-xs' type='button' data-eventid='" + r2d[i].id + "' data-toggle='modal' href='#myModal1'>Cover</button></div></li>"
             )
-            // $ ('#myModal1').data(r2d[i].id);
-            //if odd, set class? timeline inverted
-            // if (i%2 !== 0) {
-            // console.log(i)
-            // $('.timeline_li').attr('class', 'timeline-inverted')
-            // $('.timeline li').attr('class', 'timeline-li')
-            // }
-            // $('.timeline-heading').append("<h4 class='timeline-title'>" + r2d[i].title + "</h4>"); //text(r2d[i].title);
-            // $('.heroInfo').append("<li><p>" + newDate + "</p></li>")
-            // $('.heroInfo').append("<li><p>" + r2d[i].title + "</p><p>" + $.format.date(r2d[i].start, "MMMM, yyyy") + "</p><p>" + r2d[i].description + "</p></li>")
           }
           $ ('ul.timeline li:even').addClass('timeline-inverted')
-
-          // var rect = timeline.select("rect").data(data).enter();
-          // var newRect = rect.append("rect")
           console.log(entityIds);
-          // console.log(res2.data.results[5].description)
         })
       }).fail(function(response){
-        // console.log(response)
-        // console.log("failed")
+        console.log("fail")
         $('.heroInput').attr({'placeholder': 'Sorry, try a different superhero'})
       });
       // Clear form
@@ -110,7 +75,6 @@ $(document).ready(function(){
     var button = $(event.relatedTarget)
     var eventId = button.data('eventid')
     var modal = $(this)
-    // http://gateway.marvel.com:80/v1/public/events/270/comics?issueNumber=1&apikey=e687d607d622b25c31d6ae38f2f42597
     var url = "http://gateway.marvel.com:80/v1/public/events/" + eventId + "/comics?issueNumber=1&ts=" + ts + "&apikey=e687d607d622b25c31d6ae38f2f42597&hash=" + hash;
     $.ajax({
       url: url,
@@ -129,13 +93,5 @@ $(document).ready(function(){
     }).fail(function(){
       console.log("fail!")
     });
-    // console.log(eventId);
-    // var button = $(event.relatedTarget) // Button that triggered the modal
-    // var recipient = button.data('whatever') // Extract info from data-* attributes
-    // // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-    // // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
-    // var modal = $(this)
-    // modal.find('.modal-title').text('New message to ' + recipient)
-    // modal.find('.modal-body input').val(recipient)
   })
 });
