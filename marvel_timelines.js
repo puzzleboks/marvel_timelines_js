db55d7ed22b3f860d991452f2deb5c400f1bf5a9
lon phelps <puzzleboks@gmail.com>
2015-09-18 09:59:21 -0400

    Fixed bugs and added some error-checking

--- marvel_timelines.js
+++ marvel_timelines.js
@@ -10,7 +10,7 @@ $(document).ready(function(){
       var preHash = ts + privKey + pubKey;
       hash = CryptoJS.MD5(preHash).toString();
       var url = "http://gateway.marvel.com:80/v1/public/characters?name=" + name + "&ts=" + ts + "&apikey=e687d607d622b25c31d6ae38f2f42597&hash=" + hash
-
+      console.log(url)
       $.ajax({
         url: url,
         type: "get",
@@ -19,6 +19,7 @@ $(document).ready(function(){
         //if doesn't exist, then try again?
         var etag = response.etag;
         var heroId = response.data.results[0].id;
+        //if image path is null
         var myImgPath = response.data.results[0].thumbnail.path + "/landscape_incredible.jpg";
         var limit = 50;
 
@@ -81,10 +82,14 @@ $(document).ready(function(){
 
           // var rect = timeline.select("rect").data(data).enter();
           // var newRect = rect.append("rect")
-          // console.log(entityIds);
+          console.log(entityIds);
           // console.log(res2.data.results[5].description)
         })
-      })
+      }).fail(function(response){
+        // console.log(response)
+        // console.log("failed")
+        $('.heroInput').attr({'placeholder': 'Sorry, try a different superhero'})
+      });
       // Clear form
       event.target.value = "";
     }
@@ -101,9 +106,17 @@ $(document).ready(function(){
       type: "get",
       dataType: "json"
     }).done(function(res){
-      modal.find('.modal-body img').attr('src', res.data.results[0].thumbnail.path + ".jpg")
-      //close and delete picture?
-      console.log(res.data.results[0].thumbnail.path)
+      console.log(eventId);
+      if(res.data.results.length === 0){
+        modal.find('.modal-title').html("");
+        modal.find('.modal-body img').attr('src', "http://dummyimage.com/300x450/ebebeb/000000.png&text=No+image+available")
+        console.log("No data")
+      }else{
+        modal.find('.modal-title').html(res.data.results[0].title);
+        modal.find('.modal-body img').attr('src', res.data.results[0].thumbnail.path + "/portrait_uncanny.jpg")
+      }
+    }).fail(function(){
+      console.log("fail!")
     });
     // console.log(eventId);
     // var button = $(event.relatedTarget) // Button that triggered the modal
